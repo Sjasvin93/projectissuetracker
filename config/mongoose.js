d7d78d3d05aa8cbd666e42a6c2502b77c1a9daf1
unknown <sjasvin9493@gmail.com>
2022-06-01 18:59:10 +0530

    netlify

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,18 +1,7 @@
 //mongoose library
 const mongoose = require('mongoose');
 
-const DB = 'mongodb+srv://sjasvin9493:Jasvin@1993@cluster0.6e9yy.mongodb.net/project_issue_tracker_db?retryWrites=true&w=majority';
-
-//connect to the todo_list database
-mongoose.connect(DB,
-    { 
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        // useCreateIndex: true,
-        // useFindAndModify: false 
-    }).then(()=>{
-        console.log(" Mongoose is connected");
-    }).catch((err)=>console.log(" Mongoose is not connected"));
+mongoose.connect('mongodb://localhost/project_issue_tracker_db');
 
 
 //acquire the connection to check if its successful or not
@@ -27,3 +16,21 @@ db.once('open', function(){
 });
 
 module.exports = db;
+
+
+
+
+
+// const DB = 'mongodb+srv://sjasvin9493:Jasvin@1993@cluster0.6e9yy.mongodb.net/project_issue_tracker_db?retryWrites=true&w=majority';
+
+
+//connect to the todo_list database
+// mongoose.connect(DB,
+//     { 
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         // useCreateIndex: true,
+//         // useFindAndModify: false 
+//     }).then(()=>{
+//         console.log(" Mongoose is connected");
+//     }).catch((err)=>console.log(" Mongoose is not connected"));
